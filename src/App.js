45d8471288ec52fe41eddc28882a278ab5644ed9
Kallyas <kallyasmedia@gmail.com>
2021-06-13 15:34:09 +0300

    refactor code: Add deps

--- src/App.js
+++ src/App.js
@@ -1,85 +1,122 @@
 import { useState, useEffect } from 'react'
-import './App.css';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
-const initialState = [
-   
-]
-
-function App() {
-  const [tasks, setTasks] = useState(initialState)
-  const [showAddTask, setShowAddTask] = useState(true)
-
-    const addTask = async (task) => {
-      const res = await fetch("https://cr-tasks-api.herokuapp.com/tasks", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(task)
-      })
-
-      const data = await res.json()
-      setTasks([...tasks, data])
-    }
-
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-   const getTasks = async () => {
-     const response = await fetchTasks()
-     setTasks(response)
-   }
-   getTasks()
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
   }, [])
 
+  // Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('https://cr-tasks-api.herokuapp.com/tasks')
     const data = await res.json()
+
     return data
   }
 
+  // Fetch Task
   const fetchTask = async (id) => {
     const res = await fetch(`https://cr-tasks-api.herokuapp.com/tasks/${id}`)
     const data = await res.json()
+
     return data
   }
 
-  const deleteTask = async (id) => {
-    await fetch(`https://cr-tasks-api.herokuapp.com/tasks/${id}`,
-    {
-      method: "DELETE"
-    })
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
-
-  const toggleReminder = async (id) => {
-    const taskToggle = await fetchTask(id)
-    const updateTask = {...taskToggle, reminder: !taskToggle.reminder}
-
-    const res = await fetch(`https://cr-tasks-api.herokuapp.com/tasks/${id}`, {
-      method: "PUT",
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch('https://cr-tasks-api.herokuapp.com/tasks', {
+      method: 'POST',
       headers: {
-        "Content-type": "application/json"
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify(updateTask)
+      body: JSON.stringify(task),
     })
 
     const data = await res.json()
 
-    setTasks(tasks.map(task => task.id === id ? {...task, reminder: !data.reminder} : task))
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
+
+  // Delete Task
+  const deleteTask = async (id) => {
+    const res = await fetch(`https://cr-tasks-api.herokuapp.com/tasks/${id}`, {
+      method: 'DELETE',
+    })
+    //We should control the response status to decide if we will change the state or not.
+    res.status === 200
+      ? setTasks(tasks.filter((task) => task.id !== id))
+      : alert('Error Deleting This Task')
+  }
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`https://cr-tasks-api.herokuapp.com/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
+  }
+
   return (
-    <div className="container">
-      <Header 
-      title="Task Tracker" 
-      onAddBtn={() => setShowAddTask(!showAddTask)}
-      showAdd={!showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/> }
-      {tasks.length > 0 ? <Tasks onToggle={toggleReminder} tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to show!'}
-    </div>
-  );
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+            </>
+          )}
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
